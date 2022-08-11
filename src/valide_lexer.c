6bb66175e1ff93cccce46715a89b45b2fe61a404
Vincent-Barbier-code <vincentpcrog@hotmail.com>
2022-08-11 23:16:47 +0200

    Add pip validation

--- src/valide_lexer.c
+++ src/valide_lexer.c
@@ -6,7 +6,7 @@
 /*   By: vbarbier <vbarbier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/08 16:45:27 by vbarbier          #+#    #+#             */
-/*   Updated: 2022/08/11 23:06:13 by vbarbier         ###   ########.fr       */
+/*   Updated: 2022/08/11 23:16:19 by vbarbier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,27 @@ int	valide_redir(t_lexer *tmp_lexer)
 	return (1);
 }
 
+int	valide_pipe(t_lexer *tmp_lexer)
+{
+	t_pars_error	error;
+
+	if (tmp_lexer->type == PIPE)
+	{	
+		while (tmp_lexer->next && tmp_lexer->next->type == SPC)
+			tmp_lexer = tmp_lexer->next;
+		if (!tmp_lexer->next || !tmp_lexer->pre || \
+		tmp_lexer->next->type == PIPE || tmp_lexer->next->type == REDIR ||
+		tmp_lexer->pre->type == PIPE || tmp_lexer->pre->type == REDIR)
+		{
+			error.str = "|";
+			error.i = REDIR;
+			print_error(error);
+			return (0);
+		}
+	}
+	return (1);
+}
+
 int	valide_lexer(t_lexer **deb_lexer)
 {
 	t_lexer	*tmp_lexer;
@@ -46,7 +67,8 @@ int	valide_lexer(t_lexer **deb_lexer)
 	while (tmp_lexer)
 	{
 		//valide_cmd();
-		// valide_pipe();
+		if (!valide_pipe(tmp_lexer))
+			return (0);
 		// valide_quote();
 		if (!valide_redir(tmp_lexer))
 			return(0);
