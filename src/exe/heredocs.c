57d26413d08be9824fc499109c0c7be9b59db77f
Wisterd <lawenna.athonil@gmail.com>
2022-09-03 17:59:23 +0200

    commenting heredocs so it compiles

--- src/exe/heredocs.c
+++ src/exe/heredocs.c
@@ -1,19 +1,22 @@
 #include "../../inc/minishell.h"
 
+//filename limit = 255 char
+/*
 char	*create_temp_file_name()
 {
 	char	*alpha;
-	int		fd;
+	int		available;
 	char	*file_name;
 	int		i;
 	int		j;
 
 	alpha = "abcdefghijklmnopqrstuvwxyz";
-	fd = -1;
-	
-	while (fd == -1)
+	available = -1;
+	file_name = alpha[i];
+	while (available == 1)
 	{
-		fd = access(file_name, F_OK);
-
+		available = access(file_name, F_OK);
+		
 	}
 }
+*/
